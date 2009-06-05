ac81a839e4010bdb9b2e293fc3fddb97bde2c0a6
sanjiv@sanjiv-notebook <sanjiv@sanjiv-notebook>
2009-06-05 11:24:02 +0530

    Basic setuptools package using paste

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='geoalchemy',
+      version=version,
+      description="Geospatial data types support for SQLAlchemy",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='geo gis sqlalchemy orm',
+      author='Sanjiv Singh',
+      author_email='singhsanjivk@gmail.com',
+      url='http://gsoc.turbogears.org/',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )

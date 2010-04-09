ce865070d3aa4e229b787415a290464f6fde260a
tsauerwein <tobias.sauerwein@camptocamp.com>
2010-04-09 15:21:17 +0200

    use spatial index for Spatialite

--- geoalchemy/spatialite.py
+++ geoalchemy/spatialite.py
@@ -1,9 +1,11 @@
 from sqlalchemy import select, func
+from sqlalchemy.sql import and_, text, table, column 
 
 from geoalchemy.base import SpatialComparator, PersistentSpatialElement
 from geoalchemy.dialect import SpatialDialect 
 from geoalchemy import functions
 from geoalchemy.mysql import mysql_functions
+from geoalchemy.geometry import GeometryExtensionColumn
 
 
 class SQLiteComparator(SpatialComparator):
@@ -45,7 +47,27 @@ class sqlite_functions(mysql_functions):
     
     @staticmethod
     def _within_distance(compiler, geom1, geom2, distance):
-        return func.Distance(geom1, geom2) <= distance
+        if isinstance(geom1, GeometryExtensionColumn) and geom1.type.spatial_index:
+            """If querying on a geometry column that also has a spatial index,
+            then make use of this index.
+            
+            see: http://www.gaia-gis.it/spatialite/spatialite-tutorial-2.3.1.html#t8 and
+            http://groups.google.com/group/spatialite-users/browse_thread/thread/34609c7a711ac92d/7688ced3f909039c?lnk=gst&q=index#f6dbc235471574db
+            """
+            return and_(
+                        func.Distance(geom1, geom2) <= distance,
+                        table(geom1.table.fullname, column("rowid")).c.rowid.in_(
+                            select([table("idx_%s_%s" % (geom1.table.fullname, geom1.key), column("pkid")).c.pkid]).where(
+                                and_(text('xmin') >= func.MbrMinX(geom2) - distance,
+                                and_(text('xmax') <= func.MbrMaxX(geom2) + distance,
+                                and_(text('ymin') >= func.MbrMinY(geom2) - distance,
+                                     text('ymax') <= func.MbrMaxY(geom2) + distance)))
+                                )
+                            )
+                        )
+            
+        else:
+            return func.Distance(geom1, geom2) <= distance
 
 
 class SQLiteSpatialDialect(SpatialDialect):
@@ -77,8 +99,13 @@ class SQLiteSpatialDialect(SpatialDialect):
         return SQLitePersistentSpatialElement(wkb_element)
     
     def handle_ddl_before_drop(self, bind, table, column):
+        bind.execute(select([func.DisableSpatialIndex(table.name, column.name)]).execution_options(autocommit=True))
+        bind.execute("DROP TABLE idx_%s_%s" % (table.name, column.name));
+        
         bind.execute(select([func.DiscardGeometryColumn(table.name, column.name)]).execution_options(autocommit=True))
     
+    
+
     def handle_ddl_after_create(self, bind, table, column):
         bind.execute(select([func.AddGeometryColumn(table.name, 
                                                     column.name, 
@@ -86,6 +113,5 @@ class SQLiteSpatialDialect(SpatialDialect):
                                                     column.type.name, 
                                                     column.type.dimension)]).execution_options(autocommit=True))
         if column.type.spatial_index:
-            bind.execute("CREATE INDEX idx_%s_%s ON %s(%s)" % 
-                            (table.name, column.name, table.name, column.name))
+            bind.execute("SELECT CreateSpatialIndex('%s', '%s')" % (table.name, column.name));
             bind.execute("VACUUM %s" % table.name)

50f9cf8c27b1c35b8a10041959a99142842fc43d
tsauerwein <tobias.sauerwein@camptocamp.com>
2010-06-11 15:48:37 +0200

    PostGIS: also quote index and column names to support 'quoted identifiers' (see http://www.postgresql.org/docs/current/static/sql-syntax-lexical.html#SQL-SYNTAX-IDENTIFIERS)

--- geoalchemy/postgis.py
+++ geoalchemy/postgis.py
@@ -117,10 +117,10 @@ class PGSpatialDialect(SpatialDialect):
                                                     column.type.name, 
                                                     column.type.dimension)]).execution_options(autocommit=True))
         if column.type.spatial_index:
-            bind.execute("CREATE INDEX idx_%s_%s ON \"%s\".\"%s\" USING GIST (%s GIST_GEOMETRY_OPS)" % 
+            bind.execute("CREATE INDEX \"idx_%s_%s\" ON \"%s\".\"%s\" USING GIST (%s GIST_GEOMETRY_OPS)" % 
                             (table.name, column.name, (table.schema or 'public'), table.name, column.name))
             
         if not column.nullable:
-            bind.execute("ALTER TABLE \"%s\".\"%s\" ALTER COLUMN %s SET not null" % 
+            bind.execute("ALTER TABLE \"%s\".\"%s\" ALTER COLUMN \"%s\" SET not null" % 
                             ((table.schema or 'public'), table.name, column.name))
             

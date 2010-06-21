cbad9191fe119eeee3fa3e89bd6c8e42b34fc7c3
tsauerwein <tobias.sauerwein@camptocamp.com>
2010-06-21 10:04:10 +0200

    DialectManager.get_spatial_dialect for MSDialect no longer fails

--- geoalchemy/tests/test_dialect.py
+++ geoalchemy/tests/test_dialect.py
@@ -5,6 +5,7 @@ from sqlalchemy.dialects.sqlite.base import SQLiteDialect
 from sqlalchemy.dialects.mysql.base import MySQLDialect
 from sqlalchemy.dialects.oracle.base import OracleDialect
 from sqlalchemy.dialects.mssql.base import MSDialect
+from sqlalchemy.dialects.firebird.base import FBDialect
 from sqlalchemy.dialects.postgresql.psycopg2 import PGDialect_psycopg2
 
 from geoalchemy.dialect import DialectManager
@@ -14,6 +15,7 @@ from geoalchemy.spatialite import SQLiteSpatialDialect
 from geoalchemy.oracle import OracleSpatialDialect
 from geoalchemy.functions import parse_clause
 from geoalchemy.base import WKTSpatialElement
+from geoalchemy.mssql import MSSpatialDialect
 
 class TestDialectManager(TestCase):
 
@@ -23,12 +25,13 @@ class TestDialectManager(TestCase):
         ok_(isinstance(DialectManager.get_spatial_dialect(MySQLDialect()), MySQLSpatialDialect))
         ok_(isinstance(DialectManager.get_spatial_dialect(SQLiteDialect()), SQLiteSpatialDialect))
         ok_(isinstance(DialectManager.get_spatial_dialect(OracleDialect()), OracleSpatialDialect))
+        ok_(isinstance(DialectManager.get_spatial_dialect(MSDialect()), MSSpatialDialect))
         spatial_dialect2 = DialectManager.get_spatial_dialect(PGDialect_psycopg2())
         ok_(spatial_dialect is spatial_dialect2, "only one instance per dialect should be created")
     
     @raises(NotImplementedError)
     def test_get_spatial_dialect_unknown_dialect(self):
-        DialectManager.get_spatial_dialect(MSDialect())
+        DialectManager.get_spatial_dialect(FBDialect())
         
     def test_parse_clause(self):
         ok_(isinstance(parse_clause('POINT(0 0)', None), WKTSpatialElement))

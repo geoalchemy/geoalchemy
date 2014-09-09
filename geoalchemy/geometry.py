f63ab0fa15ed85357fad840591f8c7236b1f33f1
anthony <anthonypt87@gmail.com>
2014-09-08 23:41:38 -0400

    Partially reverting whitespace changes from 869d1c12237e7ceeab30400a5ffaca45fb57a045

--- geoalchemy/geometry.py
+++ geoalchemy/geometry.py
@@ -15,12 +15,12 @@ from geoalchemy.functions import functions
 class Geometry(GeometryBase):
     """Geometry column type. This is the base class for all other
     geometry types like Point, LineString, Polygon, etc.
-
+    
     Converts bind/result values to/from a dialect specific persistent
     geometry value.
-
+    
     """
-
+    
     def result_processor(self, dialect, coltype=None):
         def process(value):
             if value is not None:
@@ -29,15 +29,15 @@ class Geometry(GeometryBase):
                 return value
         return process
 
-# other datatypes can be added as needed, which
+# other datatypes can be added as needed, which 
 # currently only affect DDL statements.
 
 class Point(Geometry):
     name = 'POINT'
-
+    
 class Curve(Geometry):
     name = 'CURVE'
-
+    
 class LineString(Curve):
     name = 'LINESTRING'
 
@@ -57,18 +57,18 @@ class GeometryCollection(Geometry):
     name = 'GEOMETRYCOLLECTION'
 
 class GeometryDDL(object):
-    """A DDL extension which integrates SQLAlchemy table create/drop
+    """A DDL extension which integrates SQLAlchemy table create/drop 
     methods with AddGeometryColumn/DropGeometryColumn functions of
     spatial databases.
-
+    
     Usage::
-
+    
         sometable = Table('sometable', metadata, ...)
-
+        
         GeometryDDL(sometable)
 
         sometable.create()
-
+    
     """
 
     try:
@@ -81,7 +81,7 @@ class GeometryDDL(object):
         # SQLALchemy 0.7
         use_event = True
         columns_attribute = 'columns'
-
+    
     def __init__(self, table):
         if self.use_event:
             self._event.listen(table, 'before_create', self.before_create)
@@ -93,12 +93,12 @@ class GeometryDDL(object):
                       'before-drop', 'after-drop'):
                 table.ddl_listeners[e].append(self)
         self._stack = []
-
+        
     def __call__(self, event, table, bind):
         spatial_dialect = DialectManager.get_spatial_dialect(bind.dialect)
         if event in ('before-create', 'before-drop'):
-            """Remove geometry column from column list (table._columns), so that it
-            does not show up in the create statement ("create table tab (..)").
+            """Remove geometry column from column list (table._columns), so that it 
+            does not show up in the create statement ("create table tab (..)"). 
             Afterwards (on event 'after-create') restore the column list from self._stack.
             """
             regular_cols = [c for c in table.c if not isinstance(c.type, Geometry)]
@@ -110,10 +110,10 @@ class GeometryDDL(object):
             if event == 'before-drop':
                 for c in gis_cols:
                     spatial_dialect.handle_ddl_before_drop(bind, table, c)
-
+                
         elif event == 'after-create':
             setattr(table, self.columns_attribute, self._stack.pop())
-
+            
             for c in table.c:
                 if isinstance(c.type, Geometry):
                     spatial_dialect.handle_ddl_after_create(bind, table, c)
@@ -141,14 +141,14 @@ class GeometryDDL(object):
 
 
 class SpatialAttribute(AttributeExtension):
-    """Intercepts 'set' events on a mapped instance attribute and
+    """Intercepts 'set' events on a mapped instance attribute and 
     converts the incoming value to a GIS expression.
-
+    
     """
-
+    
     def set(self, state, value, oldvalue, initiator):
         return _to_gis(value, self.__get_srid(initiator))
-
+ 
     def __get_srid(self, initiator):
         """Returns the SRID used for the geometry column that is connected
         to this SpatialAttribute instance."""
@@ -156,10 +156,10 @@ class SpatialAttribute(AttributeExtension):
             return initiator.parent_token.columns[0].type.srid
         except Exception:
             return None
-
+ 
 class GeometryExtensionColumn(Column):
     pass
-
+        
 @compiles(GeometryExtensionColumn)
 def compile_column(element, compiler, **kw):
     if isinstance(element.table, (Table, Alias)):
@@ -171,38 +171,38 @@ def compile_column(element, compiler, **kw):
                     "compatible with %s dialect. Defaulting back to WKB"
                     % compiler.dialect.name, exc.SAWarning)
             return compiler.process(functions.wkb(element))
-
+        
     return compiler.visit_column(element, **kw)
-
-
+     
+            
 def GeometryColumn(*args, **kw):
     """Define a declarative column property with GIS behavior.
-
+    
     This just produces orm.column_property() with the appropriate
     extension and comparator_factory arguments.  The given arguments
     are passed through to Column.  The declarative module extracts
     the Column for inclusion in the mapped table.
-
-    This method can also be used for non-declarative mappings to
+    
+    This method can also be used for non-declarative mappings to 
     set the properties for a geometry column when defining the mapping.
-
+    
     """
     if kw.has_key("comparator"):
         comparator = kw.pop("comparator")
     else:
         comparator = SpatialComparator
-
+    
     if isinstance(args[0], GeometryExtensionColumn):
         # if used for non-declarative, use the column of the table definition
         column = args[0]
         args = args[1:]
     else:
         # if used for declarative, create a new column
-        column = GeometryExtensionColumn(*args, **kw)
-
+        column = GeometryExtensionColumn(*args, **kw) 
+    
     return column_property(
-        column,
-        extension=SpatialAttribute(),
+        column, 
+        extension=SpatialAttribute(), 
         comparator_factory=comparator
     )
 
